d06aaaa096af39dec39b9929bbef3a5b5df1c47c
rentverlo-dotcom <rentverlo@gmail.com>
2025-12-24 01:40:34 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import './globals.css'
 
+export const metadata = {
+  title: 'Verlo | El Futuro del Alquiler',
+  description: 'Alquila con libertad total',
+}
+
 export default function RootLayout({
   children,
 }: {
