6a4a53bbc86a9f73cde5ffca4e8c6c995f8667ce
rentverlo-dotcom <rentverlo@gmail.com>
2025-12-25 20:03:56 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function HomePage() {
 >
   <div className="container">
     <h1 className="hero-title">
-      Alquilá con libertad total.
+      Alquilá sin inmobiliarias.
     </h1>
 
     <p
