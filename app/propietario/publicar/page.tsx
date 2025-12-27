6f3d5fb5d7eb83a88c71074c8b4c9d63d4a03171
rentverlo-dotcom <rentverlo@gmail.com>
2025-12-27 11:49:10 -0300

    Create page.tsx

---
+++ app/propietario/publicar/page.tsx
@@ -0,0 +1,9 @@
+import PropertyForm from '@/components/properties/PropertyForm'
+
+export default function PublicarPage() {
+  return (
+    <main className="max-w-3xl mx-auto py-10">
+      <PropertyForm />
+    </main>
+  )
+}
