e7aad5074160ebb59315af7a79ecbfa4af0402a2
rentverlo-dotcom <rentverlo@gmail.com>
2025-12-27 20:02:57 -0300

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,7 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs'
+// lib/supabase/client.ts
+import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createBrowserClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
