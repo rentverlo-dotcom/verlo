d95e4f441946e7f51a3ced45ea82e691f7070c7b
rentverlo-dotcom <rentverlo@gmail.com>
2026-02-07 12:32:15 -0300

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
-export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
+export function supabaseServer() {
+  return createRouteHandlerClient({ cookies })
+}
