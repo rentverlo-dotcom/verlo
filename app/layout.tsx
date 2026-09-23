91119b0986c5c6587594b00f2caf333578ec1bbc
Juan Manuel Oddone <juancho12oddone@gmail.com>
2026-09-23 11:46:19 -0300

    Fix PostHog browser bootstrap

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,24 +66,51 @@ export default function RootLayout({
         <PwaRegister />
 
         {POSTHOG_ENABLED ? (
-          <Script
-            id="posthog-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey getNextSurveyStep identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset resetSessionId get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-                posthog.init(${JSON.stringify(POSTHOG_KEY)}, {
-                  api_host: ${JSON.stringify(POSTHOG_HOST)},
-                  autocapture: true,
-                  capture_pageview: true,
-                  capture_pageleave: true,
-                  session_recording: {
-                    maskAllInputs: true
-                  }
-                });
-              `,
-            }}
-          />
+          <>
+            <Script
+              id="posthog-library"
+              src={`${POSTHOG_HOST.replace(
+                '.i.posthog.com',
+                '-assets.i.posthog.com'
+              )}/static/array.js`}
+              strategy="afterInteractive"
+            />
+
+            <Script
+              id="posthog-analytics"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function initPostHog(attempt) {
+                    if (
+                      window.posthog &&
+                      typeof window.posthog.init === 'function'
+                    ) {
+                      window.posthog.init(
+                        ${JSON.stringify(POSTHOG_KEY)},
+                        {
+                          api_host: ${JSON.stringify(POSTHOG_HOST)},
+                          autocapture: true,
+                          capture_pageview: true,
+                          capture_pageleave: true,
+                          session_recording: {
+                            maskAllInputs: true
+                          }
+                        }
+                      );
+                      return;
+                    }
+
+                    if (attempt < 50) {
+                      window.setTimeout(function () {
+                        initPostHog(attempt + 1);
+                      }, 100);
+                    }
+                  })(0);
+                `,
+              }}
+            />
+          </>
         ) : null}
 
         <Script
